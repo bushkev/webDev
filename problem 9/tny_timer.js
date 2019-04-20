2f26bf74e054a1e346b38852c73c56ab9d0e52b2
bushkev <32338133+bushkev@users.noreply.github.com>
2019-04-19 21:54:37 -0400

    Add files via upload

---
+++ problem 9/tny_timer.js
@@ -0,0 +1,43 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Review Assignment
+
+   Event Timer
+   Author: Kevin Bush
+   Date:   12 April 2018
+
+*/
+showClock();
+setInterval("showClock()", 1000)
+
+function showClock () {
+	var thisDay = new Date();
+	var localDate = thisDay.toLocaleDateString();
+    var localTime = thisDay.toLocaleTimeString();
+	
+	document.getElementById("currentTime").innerHTML = "<span>"+localDate+"</span><span>"+localTime+"</span>";
+	
+	var j4Date = nextJuly4(thisDay);
+	j4Date.setHours(21);
+	var days = (j4Date-thisDay)/(1000*60*60*24);
+	var hrs = (days - Math.floor(days))*24;
+	var mins= (hrs - Math.floor(hrs))*60;
+	var secs= (mins - Math.floor(mins))*60;
+	
+	window.alert(Math.floor(days));
+	
+	document.getElementById("dLeft").innerHTML = Math.floor(days);
+	document.getElementById("hLeft").textContent = Math.floor(hrs);
+	document.getElementById("mLeft").textContent = Math.floor(mins);
+	document.getElementById("sLeft").textContent = Math.floor(secs);
+}
+
+function nextJuly4(currentDate) {
+   var cYear = currentDate.getFullYear();
+   var jDate = new Date("July 4, 2018");
+   jDate.setFullYear(cYear);
+   if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
+   return jDate;
+}
